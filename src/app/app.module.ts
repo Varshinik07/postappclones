f56959e94d260e88e7229cb430853b2a18d8d33e
Varshini <varshuk01@gmail.com>
2022-10-10 19:59:21 +0530

    postapp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { PostappComponent } from './postapp/postapp.component';
-
+import {RouterModule,Routes} from '@angular/router';
+const appRoutes:Routes=[
+  {
+    path:"",component:PostappComponent
+  },
+]
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,7 +19,8 @@ import { PostappComponent } from './postapp/postapp.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
