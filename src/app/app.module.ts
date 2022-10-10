63e6563ea3e9172576278e6ca93e5603cfaf64be
Varshini <varshuk01@gmail.com>
2022-10-10 19:54:44 +0530

    postapp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { PostappComponent } from './postapp/postapp.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    PostappComponent
   ],
   imports: [
     BrowserModule,
